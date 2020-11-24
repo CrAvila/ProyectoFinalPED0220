f4793335b543e6a47b836b9ee08243e68fdcfbae
SamVargas14 <00037720@uca.edu.sv>
2020-11-24 15:32:01 -0600

    minor bug fixes

--- abordaje.hpp
+++ abordaje.hpp
@@ -29,44 +29,49 @@ void Abordar_pasajeros(puerta, vector <cliente>, vector <cliente>);
 
 //Funciones ejecutadas directamente por el menu 
 void Crear_puerta(vector <cliente> pasajeros , vector <cliente> pasajerosDiscapacitados){
-    cliente uncl;//un cliente
-    int n = 1, o = 0;//contador y opcion 
-    //variable donde se guardara el nombre del destino
-    puerta unapuerta;
+    if (pasajeros.empty() && pasajerosDiscapacitados.empty()){
+        cout<<"\nNo hay ningun clientes para abordar\n\n";
+    }
+    else{
+        cliente uncl;//un cliente
+        int n = 1, o = 0;//contador y opcion 
+        //variable donde se guardara el nombre del destino
+        puerta unapuerta;
 
-    unapuerta.destino = ciudades[pasajeros.front().rutaVuelo.pares.front().destino].ciudad; //asignando el destino del primer nodo
-    
-    cout<<endl<<"++++++++++ PUERTAS DE ABORDAJE +++++++++"<<endl;
-    //Se imprime la primera puerta
-    cout<<"Puerta "<<n++<<" con destino a "<<unapuerta.destino<<endl;
-    puertas.push_back(unapuerta);
-    /*recorremos la lista comparando los destinos para sacar solo los destinos existentes
-    si estos destinos son diferentes al anterior se imprime como una puerta nueva*/
-    for (auto i = 0; i < pasajeros.size(); i++) {
-        uncl = pasajeros[i];
-        if (!(unapuerta.destino == ciudades[uncl.rutaVuelo.pares.front().destino].ciudad)){
-        unapuerta.destino = ciudades[uncl.rutaVuelo.pares.front().destino].ciudad;
+        unapuerta.destino = ciudades[pasajeros.front().rutaVuelo.pares.front().destino].ciudad; //asignando el destino del primer nodo
+        
+        cout<<endl<<"++++++++++ PUERTAS DE ABORDAJE +++++++++"<<endl;
+        //Se imprime la primera puerta
         cout<<"Puerta "<<n++<<" con destino a "<<unapuerta.destino<<endl;
         puertas.push_back(unapuerta);
+        /*recorremos la lista comparando los destinos para sacar solo los destinos existentes
+        si estos destinos son diferentes al anterior se imprime como una puerta nueva*/
+        for (auto i = 0; i < pasajeros.size(); i++) {
+            uncl = pasajeros[i];
+            if (!(unapuerta.destino == ciudades[uncl.rutaVuelo.pares.front().destino].ciudad)){
+            unapuerta.destino = ciudades[uncl.rutaVuelo.pares.front().destino].ciudad;
+            cout<<"Puerta "<<n++<<" con destino a "<<unapuerta.destino<<endl;
+            puertas.push_back(unapuerta);
+            }
         }
-    }
-    bool c = true;
-    do
-    {
-    //debe ingresar uno de los destinos previamente mostrados
-    cout<<endl<<"Ingrese el Numero de la puerta del vuelo que desea abordar:"<<endl;
-    cin>>o;cin.ignore();
-    if ( o < 0 || o > n-1 ){
-            cout << endl;
-            cout << "Por favor intentalo otra vez." << endl;
-            cout << endl;
-        } 
-    else {
-            c = false;
+        bool c = true;
+        do
+        {
+        //debe ingresar uno de los destinos previamente mostrados
+        cout<<endl<<"Ingrese el Numero de la puerta del vuelo que desea abordar:"<<endl;
+        cin>>o;cin.ignore();
+        if ( o < 0 || o > n-1 ){
+                cout << endl;
+                cout << "Por favor intentalo otra vez." << endl;
+                cout << endl;
+            } 
+        else {
+                c = false;
+            }
         }
+        while (c);
+        Abordar_pasajeros(puertas[o-1], pasajeros, pasajerosDiscapacitados);//pasa el destino y las listas a la funcion 
     }
-    while (c);
-    Abordar_pasajeros(puertas[o-1], pasajeros, pasajerosDiscapacitados);//pasa el destino y las listas a la funcion 
 }
 
 void Abordar_pasajeros(puerta unap, vector <cliente> pasajeros , vector <cliente> pasajerosDiscapacitados){
